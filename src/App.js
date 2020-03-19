13f2c9433d4bf5bd8531e8b1856d90d26a74020e
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-03-19 11:09:10 +0100

    code App.js handleChande, handleSubmit, constructor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      valueFromFormInput: '',
+      toDoList: []
+    };
+  }
+
+handleSubmit = (event) => {
+    event.preventDefault();
+    this.state = {
+      valueFromFormInput: '',
+      toDoList: []
+    };
+
+    axios.post('http://localhost:4000/api/v1'), 
+
+    this.setState({
+      valueFromFormInput: '',
+      toDoList: [...this.state.toDoList, this.state.valueFromFormInput]
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({ valueFromFormInput: event.target.value })
+  }
+
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1> To do list</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input value={this.state.toDoList} onchange={this.handleChange} />
+            <button>Add to list</button>
+          </form>     
         </header>
       </div>
     );
